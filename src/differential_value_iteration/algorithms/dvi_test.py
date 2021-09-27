df767ed0ecb66113fc135141b569326aae605bf6
Brian Tanner <tanno@google.com>
2021-09-27 10:48:22 -0600

    Partial progress.

--- src/differential_value_iteration/algorithms/dvi_test.py
+++ src/differential_value_iteration/algorithms/dvi_test.py
@@ -16,7 +16,7 @@ _GARET1, _GARET2, _GARET3 = garet.GARET1, garet.GARET2, garet.GARET3
 
 class DVIEvaluationTest(parameterized.TestCase):
 
-  @parameterized.parameters(np.float32, np.float64)
+  @parameterized.parameters((np.float64,),)
   def test_dvi_sync_converges(self, dtype: np.dtype):
     tolerance_places = 6 if dtype is np.float32 else 10
     environment = micro.create_mrp1(dtype)
@@ -31,6 +31,7 @@ class DVIEvaluationTest(parameterized.TestCase):
     for _ in range(50):
       changes = algorithm.update()
 
+    print(f'converged to: {algorithm.get_estimates()}')
     with self.subTest('did_not_diverge'):
       self.assertFalse(algorithm.diverged())
     with self.subTest('maintained_types'):
@@ -39,91 +40,114 @@ class DVIEvaluationTest(parameterized.TestCase):
       self.assertAlmostEqual(np.sum(np.abs(changes)), 0.,
                              places=tolerance_places)
 
-  @parameterized.parameters(np.float32, np.float64)
-  def test_dvi_async_converges(self, dtype: np.dtype):
-    tolerance_places = 6 if dtype is np.float32 else 10
-    environment = micro.create_mrp1(dtype)
-    algorithm = dvi.Evaluation(
-        mrp=environment,
-        step_size=.5,
-        beta=.5,
-        initial_r_bar=.5,
-        initial_values=np.zeros(environment.num_states, dtype=dtype),
-        synchronized=False)
-
-    for _ in range(50):
-      change_sum = 0.
-      for _ in range(environment.num_states):
-        change = algorithm.update()
-        change_sum += np.abs(change)
-
-    with self.subTest('did_not_diverge'):
-      self.assertFalse(algorithm.diverged())
-    with self.subTest('maintained_types'):
-      self.assertTrue(algorithm.types_ok())
-    with self.subTest('converged'):
-      self.assertAlmostEqual(change_sum, 0., places=tolerance_places)
-
-
-class DVIControlTest(parameterized.TestCase):
-
-  @parameterized.parameters(itertools.product(
-      (micro.create_mdp1, _GARET1, _GARET2, _GARET3),
-      (np.float32, np.float64,))
-  )
-  def test_dvi_sync_converges(self,
-      mdp_constructor: Callable[[np.dtype], structure.MarkovDecisionProcess],
-      dtype: np.dtype):
-    tolerance_places = 10 if dtype == np.float64 else 5
-    environment = mdp_constructor(dtype=dtype)
-    algorithm = dvi.Control(
-        mdp=environment,
-        step_size=.1,
-        beta=.1,
-        initial_r_bar=0.,
-        initial_values=np.zeros(environment.num_states, dtype=dtype),
-        synchronized=True)
-
-    for _ in range(500):
-      changes = algorithm.update()
-
-    with self.subTest('did_not_diverge'):
-      self.assertFalse(algorithm.diverged())
-    with self.subTest('maintained_types'):
-      self.assertTrue(algorithm.types_ok())
-    with self.subTest('converged'):
-      self.assertAlmostEqual(np.max(np.abs(changes)), 0.,
-                             places=tolerance_places)
-
-  @parameterized.parameters(itertools.product(
-      (micro.create_mdp1, _GARET1, _GARET2, _GARET3),
-      (np.float32, np.float64))
-  )
-  def test_dvi_async_converges(self,
-      mdp_constructor: Callable[[np.dtype], structure.MarkovDecisionProcess],
-      dtype: np.dtype):
-    tolerance_places = 9 if dtype == np.float64 else 6
-    environment = mdp_constructor(dtype=dtype)
-    algorithm = dvi.Control(
-        mdp=environment,
-        step_size=.5,
-        beta=.5,
-        initial_r_bar=.0,
-        initial_values=np.zeros(environment.num_states, dtype=dtype),
-        synchronized=False)
-
-    for _ in range(100):
-      change_max = 0.
-      for _ in range(environment.num_states):
-        change = algorithm.update()
-        change_max = np.maximum(change_max, np.abs(change))
-
-    with self.subTest('did_not_diverge'):
-      self.assertFalse(algorithm.diverged())
-    with self.subTest('maintained_types'):
-      self.assertTrue(algorithm.types_ok())
-    with self.subTest('converged'):
-      self.assertAlmostEqual(change_max, 0., places=tolerance_places)
+  # @parameterized.parameters(np.float32, np.float64)
+  # def test_dvi_sync_converges(self, dtype: np.dtype):
+  #   tolerance_places = 6 if dtype is np.float32 else 10
+  #   environment = micro.create_mrp1(dtype)
+  #   algorithm = dvi.Evaluation(
+  #       mrp=environment,
+  #       step_size=.5,
+  #       beta=.5,
+  #       initial_r_bar=.5,
+  #       initial_values=np.zeros(environment.num_states, dtype=dtype),
+  #       synchronized=True)
+  #
+  #   for _ in range(50):
+  #     changes = algorithm.update()
+  #
+  #   with self.subTest('did_not_diverge'):
+  #     self.assertFalse(algorithm.diverged())
+  #   with self.subTest('maintained_types'):
+  #     self.assertTrue(algorithm.types_ok())
+  #   with self.subTest('converged'):
+  #     self.assertAlmostEqual(np.sum(np.abs(changes)), 0.,
+  #                            places=tolerance_places)
+  #
+  # @parameterized.parameters(np.float32, np.float64)
+  # def test_dvi_async_converges(self, dtype: np.dtype):
+  #   tolerance_places = 6 if dtype is np.float32 else 10
+  #   environment = micro.create_mrp1(dtype)
+  #   algorithm = dvi.Evaluation(
+  #       mrp=environment,
+  #       step_size=.5,
+  #       beta=.5,
+  #       initial_r_bar=.5,
+  #       initial_values=np.zeros(environment.num_states, dtype=dtype),
+  #       synchronized=False)
+  #
+  #   for _ in range(50):
+  #     change_sum = 0.
+  #     for _ in range(environment.num_states):
+  #       change = algorithm.update()
+  #       change_sum += np.abs(change)
+  #
+  #   with self.subTest('did_not_diverge'):
+  #     self.assertFalse(algorithm.diverged())
+  #   with self.subTest('maintained_types'):
+  #     self.assertTrue(algorithm.types_ok())
+  #   with self.subTest('converged'):
+  #     self.assertAlmostEqual(change_sum, 0., places=tolerance_places)
+#
+#
+# class DVIControlTest(parameterized.TestCase):
+#
+#   @parameterized.parameters(itertools.product(
+#       (micro.create_mdp1, _GARET1, _GARET2, _GARET3),
+#       (np.float32, np.float64,))
+#   )
+#   def test_dvi_sync_converges(self,
+#       mdp_constructor: Callable[[np.dtype], structure.MarkovDecisionProcess],
+#       dtype: np.dtype):
+#     tolerance_places = 10 if dtype == np.float64 else 5
+#     environment = mdp_constructor(dtype=dtype)
+#     algorithm = dvi.Control(
+#         mdp=environment,
+#         step_size=.1,
+#         beta=.1,
+#         initial_r_bar=0.,
+#         initial_values=np.zeros(environment.num_states, dtype=dtype),
+#         synchronized=True)
+#
+#     for _ in range(500):
+#       changes = algorithm.update()
+#
+#     with self.subTest('did_not_diverge'):
+#       self.assertFalse(algorithm.diverged())
+#     with self.subTest('maintained_types'):
+#       self.assertTrue(algorithm.types_ok())
+#     with self.subTest('converged'):
+#       self.assertAlmostEqual(np.max(np.abs(changes)), 0.,
+#                              places=tolerance_places)
+#
+#   @parameterized.parameters(itertools.product(
+#       (micro.create_mdp1, _GARET1, _GARET2, _GARET3),
+#       (np.float32, np.float64))
+#   )
+#   def test_dvi_async_converges(self,
+#       mdp_constructor: Callable[[np.dtype], structure.MarkovDecisionProcess],
+#       dtype: np.dtype):
+#     tolerance_places = 9 if dtype == np.float64 else 6
+#     environment = mdp_constructor(dtype=dtype)
+#     algorithm = dvi.Control(
+#         mdp=environment,
+#         step_size=.5,
+#         beta=.5,
+#         initial_r_bar=.0,
+#         initial_values=np.zeros(environment.num_states, dtype=dtype),
+#         synchronized=False)
+#
+#     for _ in range(100):
+#       change_max = 0.
+#       for _ in range(environment.num_states):
+#         change = algorithm.update()
+#         change_max = np.maximum(change_max, np.abs(change))
+#
+#     with self.subTest('did_not_diverge'):
+#       self.assertFalse(algorithm.diverged())
+#     with self.subTest('maintained_types'):
+#       self.assertTrue(algorithm.types_ok())
+#     with self.subTest('converged'):
+#       self.assertAlmostEqual(change_max, 0., places=tolerance_places)
 
 
 if __name__ == '__main__':
